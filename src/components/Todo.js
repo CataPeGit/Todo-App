7a881b7c6a266912fd272ac3078872cb4520140f
CataPeGit <74926354+CataPeGit@users.noreply.github.com>
2022-07-14 18:37:54 +0300

    Update Todo.js

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,24 +1,50 @@
-import React, { useState }  from 'react'
+import React, { useReducer, useState }  from 'react'
+import Categories from './Categories';
 import Form from './Form';
 import List from './List';
 
 const Todo = () => {
 
+    // Note useState hooks
     const [allTodoNotes, setTodoNotes] = useState([]);
     const [inputText, setInputText] = useState("");
+    const [currentNotesByCategory, setCurrentNotesByCategory] = useState([]);
+
+    // Categories useState hooks
+    const [allCategories, setAllCategories] = useState([]);
+    const [categoryInput, setCategoryInput] = useState("");
+    const [currentCategoryState, setCurrentCategoryState] = useState("All categories");
     
 
     return (
+
         <div>
+            <header>
+                <h1>Todo App</h1>
+            </header>
             <Form 
-                inputText={inputText}
+                currentCategoryState={currentCategoryState}
+                inputText={inputText} 
                 allTodoNotes={allTodoNotes}
                 setTodoNotes={setTodoNotes}
                 setInputText={setInputText}
             >  
             </Form>
 
-            <List allTodoNotes={allTodoNotes} setTodoNotes={setTodoNotes} ></List>
+            <Categories 
+                allCategories={allCategories}
+                categoryInput={categoryInput} 
+                setAllCategories={setAllCategories}  
+                setCategoryInput={setCategoryInput}
+                setCurrentCategoryState={setCurrentCategoryState}
+
+                allTodoNotes = {allTodoNotes}
+                currentNotesByCategory={currentNotesByCategory}
+                setCurrentNotesByCategory={setCurrentNotesByCategory}
+            >
+            </Categories>
+
+            <List currentCategoryState={currentCategoryState} allTodoNotes={allTodoNotes} setTodoNotes={setTodoNotes} ></List>
 
         </div>
     )
