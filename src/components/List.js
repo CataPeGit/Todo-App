960a3e50db336cb48350b8da1f66c960e345d8a6
CataPeGit <74926354+CataPeGit@users.noreply.github.com>
2022-07-16 21:47:22 +0300

    Update List.js

--- src/components/List.js
+++ src/components/List.js
@@ -1,14 +1,14 @@
 import React from 'react'
 import Note from './Note';
 
-const List = ({ allTodoNotes, setTodoNotes, currentCategoryState }) => {
+const List = ({ allTodoNotes, setTodoNotes, currentCategoryState, date, setPassed }) => {
 
     return (
         <div className='todo-container'>
             <ul className='todo-list'>
                 {
                     allTodoNotes.map((todo) => (
-                        <Note currentCategoryState={currentCategoryState} setTodoNotes={setTodoNotes} allTodoNotes={allTodoNotes} todo={todo}></Note>
+                        <Note setPassed={setPassed} date={date} currentCategoryState={currentCategoryState} setTodoNotes={setTodoNotes} allTodoNotes={allTodoNotes} todo={todo}></Note>
                         
                         )
                     )
