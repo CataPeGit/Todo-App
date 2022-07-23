dfe42f6ca8dc2bc597f13d4df368216745d1ee28
CataPeGit <74926354+CataPeGit@users.noreply.github.com>
2022-07-23 16:23:48 +0300

    Update Form.js

--- src/components/Form.js
+++ src/components/Form.js
@@ -6,20 +6,12 @@ const Form = ({ inputText, allTodoNotes, setTodoNotes, setInputText, currentCate
 
     const changeInputText = (inputElement) => {
         // handling the input text
-        // if (!inputElement.text || /^\s*$/.test(inputElement.text)){
-        //     return;
-        // }
-
         setInputText(inputElement.target.value);
     }
 
     const addNote = (inputElement) => {
         // adding a new note to the notes useState
         inputElement.preventDefault();
-        //if (!inputElement.text || /^\s*$/.test(inputElement.text)){
-        // if (!inputElement.text || /^\s*$/.test(inputElement.text)){
-        //     return;
-        // }
 
         const currentDate = new Date();
         currentDate.toLocaleDateString(); 
@@ -27,32 +19,6 @@ const Form = ({ inputText, allTodoNotes, setTodoNotes, setInputText, currentCate
         setTodoNotes([{text: inputText, taskCompleted:false, id:Math.random()*10000 % 1000, noteCategory:currentCategoryState, noteDeadline:date, noteDeadlinePassed:passed}, ...allTodoNotes]);
         setInputText("");
     }
-
-    /*
-    const deadlineHandler = (inputElement)=> {
-        const currentDate = new Date();
-
-        currentDate.toLocaleDateString(); 
-        console.log(currentDate);
-        console.log(currentDate.getDate());
-        console.log(currentDate.getMonth() + 1);
-        console.log(currentDate.getFullYear());
-        console.log(date);
-        console.log("COX");
-        console.log( (currentDate.getMonth() + 1).toString());
-        console.log((date.substring(5, 7) === (currentDate.getMonth() + 1).toString()));
-        console.log(passed);
-
-        if (date.substring(0, date.indexOf('-')) < currentDate.getFullYear().toString()) {
-            setPassed(true);
-        } else if ((date.substring(5, 7) < (currentDate.getMonth() + 1)) && (date.substring(0, date.indexOf('-')) === currentDate.getFullYear().toString())) {
-            setPassed(true);
-        } else if ((date.substring(8, 10) < (currentDate.getMonth() + 1)) && (date.substring(5, 7) === (currentDate.getMonth() + 1)) && (date.substring(0, date.indexOf('-')) === currentDate.getFullYear().toString())) {
-            setPassed(true);
-        } else {
-            setPassed(false);
-        }
-    } */
 
     return (
 
